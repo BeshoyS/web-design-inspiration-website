ca6ecd7e585c3f53b4a889da99107f7cc32612c3
Moemen Moemen <moumenyasser@gmail.com>
2021-10-07 04:31:23 +0200

    - Json package - Header menu (not finished yet)
    Signed-off-by: Moemen Moemen <moumenyasser@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-alert("Hi");
+
+
+const menuBtn = document.getElementById('menubtn');
+const menuPanel = document.getElementById('menupanel');
+const closeBtn = document.getElementById('closebtn');
+
+menuBtn.addEventListener('click',function()
+{
+
+   
+   
+     
+        menuPanel.style.right = 0;
+        menuBtn.style.zIndex=-2;
+
+      
+  
+    
+    
+})
+
+closeBtn.addEventListener('click',function()
+{
+
+  
+   
+    
+        menuPanel.style.right = getComputedStyle(document.documentElement).getPropertyValue('--menu-panel-w');
+        setTimeout(()=>{
+            menuBtn.style.zIndex=1;
+        },300);
+        
+
+
+    
+})
